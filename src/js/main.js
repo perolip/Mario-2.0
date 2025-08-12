58ad9d8a9443796fb62e9c4bf5db597b7910451e
perolip <joao.peroli@escola.pr.gov.br>
2025-08-12 09:45:12 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,111 +1,121 @@
-const mario = document.querySelector('.mario')
-const pipe = document.querySelector('.pipe')
 
-const start = document.querySelector('.start')
-const gameOver = document.querySelector('.game-over')
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
-audioStart = new Audio('./src/audio/audio_theme.mp3')
-audioGameOver = new Audio('./src/audio/audio_gameover.mp3')
+const startScreen = document.querySelector('.start');
+const gameOverScreen = document.querySelector('.game-over');
+const deathCounter = document.querySelector('.death-counter');
+const restartButton = document.querySelector('.restart-button');
+const startButton = document.querySelector('.button-start');
+const distanceCounter = document.querySelector('.distance-counter');
 
-// Nova variável para contar as mortes
-let deathCount = 0;
+let deaths = 0;
+let loopInterval;
+
+let distance = 0;
+let maxDistance = localStorage.getItem('maxDistance') ? parseInt(localStorage.getItem('maxDistance')) : 0;
+let distanceInterval;
+
+const audioStart = new Audio('./src/audio/audio_theme.mp3');
+const audioGameOver = new Audio('./src/audio/audio_gameover.mp3');
+
+const updateDistance = () => {
+    distance += 1;
+    distanceCounter.textContent = `Distância: ${distance}m | Máxima: ${maxDistance}m`;
+};
 
 const startGame = () => {
-    pipe.classList.add('pipe-animation')
-    start.style.display = 'none'
+    pipe.classList.add('pipe-animation');
+    startScreen.style.display = 'none'; // Esconde a tela de início
 
-    // audio
-    audioStart.play()
-}
+    audioStart.play();
+    loopInterval = setInterval(loop, 10);
+    
+    distance = 0;
+    distanceInterval = setInterval(updateDistance, 100);
+};
 
 const restartGame = () => {
-    gameOver.style.display = 'none'
-    pipe.style.left = ''
-    pipe.style.right = '0'
-    mario.src = './src/img/mario.gif'
-    mario.style.width = '150px'
-    mario.style.bottom = '0'
+    gameOverScreen.style.display = 'none'; // Esconde a tela de game over
+    startScreen.style.display = 'flex'; // Mostra a tela de início novamente
+    
+    // Reseta a posição do cano
+    pipe.style.left = '';
+    pipe.style.right = '0';
+    pipe.classList.remove('pipe-animation');
+    
+    // Reseta o Mario
+    mario.src = './src/img/mario.gif';
+    mario.style.width = '150px';
+    mario.style.bottom = '0';
+    mario.style.marginLeft = '0px';
 
-    start.style.display = 'none'
-
-    audioGameOver.pause()
+    audioGameOver.pause();
     audioGameOver.currentTime = 0;
 
-    audioStart.play()
+    audioStart.pause();
     audioStart.currentTime = 0;
-    
-    // Zera a contagem de mortes ao reiniciar o jogo
-    deathCount = 0;
-    document.getElementById('death-count').innerText = deathCount;
-}
+
+    distance = 0;
+    distanceCounter.textContent = `Distância: ${distance}m | Máxima: ${maxDistance}m`;
+};
 
 const jump = () => {
-    mario.classList.add('jump')
+    mario.classList.add('jump');
 
     setTimeout(() => {
-        mario.classList.remove('jump')
-    }, 800)
-}
+        mario.classList.remove('jump');
+    }, 800);
+};
 
 const loop = () => {
-    setInterval(() => {
-        const pipePosition = pipe.offsetLeft
-        const marioPosition = window
-            .getComputedStyle(mario)
-            .bottom.replace('px', ' ')
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = parseFloat(window.getComputedStyle(mario).bottom);
 
-        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-            pipe.classList.remove('pipe-animation')
-            pipe.style.left = `${pipePosition}px`
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        clearInterval(loopInterval);
+        clearInterval(distanceInterval); // Para de contar a distância
+        
+        pipe.classList.remove('pipe-animation');
+        pipe.style.left = `${pipePosition}px`;
 
-            mario.classList.remove('jump')
-            mario.style.bottom = `${marioPosition}px`
-
-            mario.src = './src/img/game-over.png'
-            mario.style.width = '80px'
-            mario.style.marginLeft = '50px'
-            
-            // Aumenta o contador de mortes
-            deathCount++;
-            document.getElementById('death-count').innerText = deathCount;
-            
-            function stopAudioStart() {
-                audioStart.pause()
-            }
-            stopAudioStart()
-            
-            audioGameOver.play()
-            
-            function stopAudio() {
-                audioGameOver.pause()
-            }
-            setTimeout(stopAudio, 7000)
-            
-            gameOver.style.display = 'flex'
-            
-            clearInterval(loop)
+        mario.classList.remove('jump');
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = './src/img/game-over.png';
+        mario.style.width = '80px';
+        mario.style.marginLeft = '50px';
+        
+        audioStart.pause();
+        audioGameOver.play();
+        
+        gameOverScreen.style.display = 'flex';
+        
+        deaths++;
+        deathCounter.textContent = `Mortes: ${deaths}`;
+        
+        // Verifica e salva a distância máxima
+        if (distance > maxDistance) {
+            maxDistance = distance;
+            localStorage.setItem('maxDistance', maxDistance);
         }
-    }, 10)
-}
-
-loop()
+        distanceCounter.textContent = `Distância: ${distance}m | Máxima: ${maxDistance}m`;
+    }
+};
 
 document.addEventListener('keypress', e => {
-    const tecla = e.key
-    if (tecla === ' ') {
-        jump()
+    if (e.key === ' ') {
+        jump();
     }
-})
+});
 
 document.addEventListener('touchstart', e => {
     if (e.touches.length) {
-        jump() 
+        jump();
     }
-})
+});
 
-document.addEventListener('keypress', e => {
-    const tecla = e.key
-    if (tecla === 'Enter') {
-        startGame()
-    }
-})
+startButton.addEventListener('click', startGame);
+restartButton.addEventListener('click', restartGame);
+
+// Atualiza a distância máxima inicial ao carregar a página
+distanceCounter.textContent = `Distância: ${distance}m | Máxima: ${maxDistance}m`;
